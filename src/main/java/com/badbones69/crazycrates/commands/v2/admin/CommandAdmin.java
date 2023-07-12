19a79d57b801be6227566a18146fb774776eb816
Ryder Belserion <no-reply@ryderbelserion.com>
2023-07-12 01:52:52 -0400

    Update current commands

--- src/main/java/com/badbones69/crazycrates/commands/v2/admin/CommandAdmin.java
+++ src/main/java/com/badbones69/crazycrates/commands/v2/admin/CommandAdmin.java
@@ -2,10 +2,9 @@ package com.badbones69.crazycrates.commands.v2.admin;
 
 import ch.jalu.configme.SettingsManager;
 import com.badbones69.crazycrates.CrazyCrates;
-import com.badbones69.crazycrates.commands.engine.CommandContext;
-import com.badbones69.crazycrates.commands.engine.CommandEngine;
-import com.badbones69.crazycrates.commands.engine.builder.CommandDataEntry;
-import com.badbones69.crazycrates.commands.engine.requirements.CommandRequirementsBuilder;
+import com.badbones69.crazycrates.commands.engine.v1.CommandContext;
+import com.badbones69.crazycrates.commands.engine.v1.CommandEngine;
+import com.badbones69.crazycrates.commands.engine.v2.builders.requirements.CommandRequirementsBuilder;
 import com.badbones69.crazycrates.api.enums.Permissions;
 import org.bukkit.plugin.java.JavaPlugin;
 
--- src/main/java/com/badbones69/crazycrates/commands/v2/admin/keys/CommandGiveKeys.java
+++ src/main/java/com/badbones69/crazycrates/commands/v2/admin/keys/CommandGiveKeys.java
@@ -2,26 +2,25 @@ package com.badbones69.crazycrates.commands.v2.admin.keys;
 
 import ch.jalu.configme.SettingsManager;
 import com.badbones69.crazycrates.CrazyCrates;
-import com.badbones69.crazycrates.commands.engine.CommandContext;
-import com.badbones69.crazycrates.commands.engine.CommandEngine;
-import com.badbones69.crazycrates.commands.engine.builder.CommandDataEntry;
-import com.badbones69.crazycrates.commands.engine.requirements.CommandRequirementsBuilder;
-import com.badbones69.crazycrates.commands.engine.sender.args.Argument;
-import com.badbones69.crazycrates.commands.engine.sender.args.builder.IntArgument;
+import com.badbones69.crazycrates.commands.engine.v1.CommandContext;
+import com.badbones69.crazycrates.commands.engine.v1.CommandEngine;
+import com.badbones69.crazycrates.commands.engine.v2.builders.requirements.CommandRequirementsBuilder;
+import com.badbones69.crazycrates.commands.engine.v2.builders.args.Argument;
+import com.badbones69.crazycrates.commands.engine.v2.builders.args.builder.IntArgument;
 import com.badbones69.crazycrates.api.enums.Permissions;
-import com.badbones69.crazycrates.commands.engine.sender.args.builder.custom.CrateArgument;
-import com.badbones69.crazycrates.commands.engine.sender.args.builder.custom.KeyArgument;
-import com.badbones69.crazycrates.commands.engine.sender.args.builder.custom.PlayerArgument;
+import com.badbones69.crazycrates.commands.engine.v2.builders.args.builder.custom.CrateArgument;
+import com.badbones69.crazycrates.commands.engine.v2.builders.args.builder.custom.KeyArgument;
+import com.badbones69.crazycrates.commands.engine.v2.builders.args.builder.custom.PlayerArgument;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class CommandAddKeys extends CommandEngine {
+public class CommandGiveKeys extends CommandEngine {
 
     private final CrazyCrates plugin = JavaPlugin.getPlugin(CrazyCrates.class);
 
     private final SettingsManager pluginConfig = this.plugin.getApiManager().getPluginConfig();
     private final SettingsManager locale = this.plugin.getApiManager().getLocale();
 
-    public CommandAddKeys() {
+    public CommandGiveKeys() {
         addAlias("give");
 
         setCommandEntryData(new CommandDataEntry());
