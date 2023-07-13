8206ad10d6eeb647ff1bbdab93ee976c9045ea36
Ryder Belserion <no-reply@ryderbelserion.com>
2023-07-13 01:08:29 -0400

    Re-organize locale

--- api/src/main/java/com/badbones69/crazycrates/api/configs/types/Locale.java
+++ api/src/main/java/com/badbones69/crazycrates/api/configs/types/Locale.java
@@ -30,73 +30,83 @@ public class Locale implements SettingsHolder {
         conf.setComment("general", header);
     }
 
-    public static final Property<String> INVALID_ACTION = newProperty("general.requirements.invalid-action", "{prefix}<gold>{value}</gold> <red>is an invalid {action}.</red>");
+    public static final Property<String> UNKNOWN_COMMAND = newProperty("general.unknown-command", "{prefix}<red>The command {command} is not known.</red>");
 
-    public static final Property<String> REQUIRED_ARGUMENT = newProperty("general.requirements.required-argument", "{prefix}<red>This argument is not optional</red>");
+    public static final Property<String> INVALID_SYNTAX = newProperty("general.invalid-syntax", "{prefix}<gold>{value}</gold> <red>is an invalid {action}.</red>");
 
-    public static final Property<String> OPTIONAL_ARGUMENT = newProperty("general.requirements.optional-argument", "{prefix}<green>This argument is optional</green>");
+    public static final Property<String> NO_PERMISSION = newProperty("general.no-permission", "{prefix}<red>You do not have permission to use that command!</red>");
 
-    public static final Property<String> TOO_MANY_ARGS = newProperty("general.requirements.too-many-args", "{prefix}<red>You put more arguments then I can handle.</red>");
+    public static final Property<String> REQUIRED_ARGUMENT = newProperty("general.checks.required-argument", "{prefix}<red>This argument is not optional</red>");
 
-    public static final Property<String> NOT_ENOUGH_ARGS = newProperty("general.requirements.not-enough-args", "{prefix}<red>You did not supply enough arguments.</red>");
+    public static final Property<String> OPTIONAL_ARGUMENT = newProperty("general.checks.optional-argument", "{prefix}<green>This argument is optional</green>");
 
-    public static final Property<String> UNKNOWN_COMMAND = newProperty("misc.unknown-command", "{prefix}<red>The command {command} is not known.</red>");
+    public static final Property<String> NOT_ENOUGH_ARGS = newProperty("general.checks.not-enough-args", "{prefix}<red>You did not supply enough arguments.</red>");
 
-    public static final Property<String> NO_TELEPORTING = newProperty("misc.no-teleporting", "{prefix}<red>You may not teleport away while opening</red> <gold>{crate}.</gold>");
+    public static final Property<String> TOO_MANY_ARGS = newProperty("general.checks.too-many-args", "{prefix}<red>You put more arguments then I can handle.</red>");
 
-    public static final Property<String> NO_COMMANDS = newProperty("misc.no-commands", "{prefix}<red>You are not allowed to use commands while opening</red> <gold>{crate}.</gold>");
+    public static final Property<String> NO_PRIZES_FOUND = newProperty("general.errors.no-prizes.found", "{prefix}<red>{crate} contains no prizes that you can win.</red>");
+
+    public static final Property<String> INTERNAL_ERROR = newProperty("general.errors.internal-error", "{prefix}<red>An internal error has occurred. Please check the console for the full error.</red>");
+
+    public static final Property<String> NO_SCHEMATICS_FOUND = newProperty("general.errors.no-schematics-found", "{prefix}<red>No schematic were found, Please re-generate them by deleting the folder or checking for errors!</red>");
+
+    public static final Property<String> PRIZE_ERROR = newProperty("general.errors.prize-error", "{prefix}<red>An error has occurred in</red> <gold>{crate}</gold> <red>for</red> <gold>#{prize}. Contact your owner!</gold>");
+
+    public static final Property<String> PLAYER_DOES_NOT_EXIST = newProperty("general.player-checks.invalid", "{prefix}<red>The player does not exist</red>");
+
+    public static final Property<String> MUST_BE_PLAYER = newProperty("general.player-checks.must-be-player", "{prefix}<red>You must be a player to use this command.</red>");
+
+    public static final Property<String> TARGET_SAME_PLAYER = newProperty("general.player-checks.target-same-player", "{prefix}<red>You cannot use this command on yourself.</red>");
+
+    public static final Property<String> MUST_BE_LOOKING_AT_BLOCK = newProperty("general.player-checks.must-be-looking-at-block", "{prefix}<red>You must be looking at a block.</red>");
+
+    public static final Property<String> MUST_BE_CONSOLE_SENDER = newProperty("general.player-checks.must-be-console-sender", "{prefix}<red>You must be using console to use this command.</red>");
+
+    public static final Property<String> TARGET_NOT_ONLINE = newProperty("general.player-checks.target-not-online", "{prefix}<red>The player</red> <gold>{player}</gold> <red>is not online.</red>");
+
+    public static final Property<String> GRABBING_OFFLINE_PLAYER = newProperty("general.player-checks.grabbing-offline-player", "{prefix}<red>Grabbing the offline player!</red> <yellow>I hope you got the name spelled right!</yellow>");
 
     public static final Property<String> NO_KEYS = newProperty("misc.no-keys", "{prefix}<red>You need a</red> {key} <red>in your hand to use</red> <gold>{crate}.</gold>");
 
     public static final Property<String> NO_VIRTUAL_KEYS = newProperty("misc.no-virtual-keys", "{prefix}<red>You need</red> {key} <red>to open</red> <gold>{crate}.</gold>");
 
+    public static final Property<String> NO_TELEPORTING = newProperty("misc.no-teleporting", "{prefix}<red>You may not teleport away while opening</red> <gold>{crate}.</gold>");
+
+    public static final Property<String> NO_COMMANDS = newProperty("misc.no-commands", "{prefix}<red>You are not allowed to use commands while opening</red> <gold>{crate}.</gold>");
+
     public static final Property<String> FEATURE_DISABLED = newProperty("misc.feature-disabled", "{prefix}<red>This feature is disabled. We have no ETA on when this will function.</red>");
-
-    public static final Property<String> NO_PRIZES_FOUND = newProperty("errors.no-prizes.found", "{prefix}<red>{crate} contains no prizes that you can win.</red>");
-
-    public static final Property<String> NO_SCHEMATICS_FOUND = newProperty("errors.no-schematics-found", "{prefix}<red>No schematic were found, Please re-generate them by deleting the folder or checking for errors!</red>");
-
-    public static final Property<String> PRIZE_ERROR = newProperty("errors.prize-error", "{prefix}<red>An error has occurred in</red> <gold>{crate}</gold> <red>for</red> <gold>#{prize}. Contact your owner!</gold>");
-
-    public static final Property<String> INTERNAL_ERROR = newProperty("errors.internal-error", "{prefix}<red>An internal error has occurred. Please check the console for the full error.</red>");
-
-    public static final Property<String> PLAYER_DOES_NOT_EXIST = newProperty("player.requirements.invalid", "{prefix}<red>The player does not exist</red>");
-
-    public static final Property<String> MUST_BE_PLAYER = newProperty("player.requirements.must-be-player", "{prefix}<red>You must be a player to use this command.</red>");
-
-    public static final Property<String> MUST_BE_CONSOLE_SENDER = newProperty("player.requirements.must-be-console-sender", "{prefix}<red>You must be using console to use this command.</red>");
-
-    public static final Property<String> MUST_BE_LOOKING_AT_BLOCK = newProperty("player.requirements.must-be-looking-at-block", "{prefix}<red>You must be looking at a block.</red>");
-
-    public static final Property<String> TARGET_NOT_ONLINE = newProperty("player.target-not-online", "{prefix}<red>The player</red> <gold>{player}</gold> <red>is not online.</red>");
-
-    public static final Property<String> TARGET_SAME_PLAYER = newProperty("player.target-same-player", "{prefix}<red>You cannot use this command on yourself.</red>");
-
-    public static final Property<String> GRABBING_OFFLINE_PLAYER = newProperty("player.grabbing-offline-player", "{prefix}<red>Grabbing the offline player!</red> <yellow>I hope you got the name spelled right!</yellow>");
-
-    public static final Property<String> NO_PERMISSION = newProperty("player.no-permission", "{prefix}<red>You do not have permission to use that command!</red>");
-
-    public static final Property<String> INVENTORY_NOT_EMPTY = newProperty("player.inventory-not-empty", "{prefix}<red>Inventory is not empty, Please make room before opening</red> <gold>{crate}.</gold>");
 
     public static final Property<String> OBTAINING_KEYS = newProperty("player.obtaining-keys", "{prefix}<gray>You have been given</gray> <gold>{amount} {key}</gold> <gray>keys.</gray>");
 
     public static final Property<String> TOO_CLOSE_TO_ANOTHER_PLAYER = newProperty("player.too-close-to-another-player", "{prefix}<red>You are too close to a player that is opening their crate.</red>");
 
-    public static final Property<String> NOT_A_CRATE = newProperty("crates.requirements.not-a-crate", "{prefix}<red>There is no crate called</red> <gold>{crate}.</gold>");
+    public static final Property<String> INVENTORY_NOT_EMPTY = newProperty("player.inventory-not-empty", "{prefix}<red>Inventory is not empty, Please make room before opening</red> <gold>{crate}.</gold>");
 
-    public static final Property<String> NOT_ON_BLOCK = newProperty("crates.not-on-block", "{prefix}<red>You must be standing on a block to use</red> <gold>{crate}.</gold>");
 
-    public static final Property<String> OUT_OF_TIME = newProperty("crates.out-of-time", "{prefix}<red>You took</red> <green>5 Minutes</green> <red>to open the</red> <gold>{crate}</gold> <red>so it closed.</red>");
-
-    public static final Property<String> PREVIEW_DISABLED = newProperty("crates.preview-disabled", "{prefix}<red>The preview for</red> <gold>{crate}</gold> <red>is currently disabled.</red>");
+    public static final Property<String> WORLD_DISABLED = newProperty("crates.world-disabled", "{prefix}<red>Crates are disabled in</red> <gold>{world}.</gold>");
 
     public static final Property<String> CRATE_ALREADY_OPEN = newProperty("crates.already-open", "{prefix}<red>You are already opening</red> <gold>{crate}.</gold>");
 
-    public static final Property<String> CRATE_IN_USE = newProperty("crates.in-use", "{prefix}<gold>{crate}</gold> <red>is already in use. Please wait until it finishes!</red>");
+    public static final Property<String> NEED_MORE_ROOM = newProperty("crates.need-more-room", "{prefix}<red>There is not enough space to open {crate} here.</red>");
 
-    public static final Property<String> CANNOT_BE_VIRTUAL_CRATE = newProperty("crates.cannot-be-a-virtual-crate", "{prefix}<gold>{crate}</gold> <red>cannot be used as a Virtual Crate. You have it set to</red> <gold>{cratetype}.</gold>");
+    public static final Property<List<String>> PHYSICAL_CRATE_CREATED = newListProperty("crates.physical-crate.created", List.of(
+            "{prefix}<gray>You have set that block to</gray> <gold>{crate}.</gold>",
+            "{prefix}<gray>To remove</gray> <gold>{crate},</gold> <gray>Shift-click break in creative to remove.</gray>"
+    ));
+
+    public static final Property<String> NOT_A_CRATE = newProperty("crates.requirements.not-a-crate", "{prefix}<red>There is no crate called</red> <gold>{crate}.</gold>");
 
     public static final Property<String> PHYSICAL_CRATE_REMOVED = newProperty("crates.physical-crate.removed", "{prefix}<gray>You have removed</gray> <gold>{id}.</gold>");
+
+    public static final Property<String> NOT_ON_BLOCK = newProperty("crates.not-on-block", "{prefix}<red>You must be standing on a block to use</red> <gold>{crate}.</gold>");
+
+    public static final Property<String> CRATE_IN_USE = newProperty("crates.in-use", "{prefix}<gold>{crate}</gold> <red>is already in use. Please wait until it finishes!</red>");
+
+    public static final Property<String> PREVIEW_DISABLED = newProperty("crates.preview-disabled", "{prefix}<red>The preview for</red> <gold>{crate}</gold> <red>is currently disabled.</red>");
+
+    public static final Property<String> OUT_OF_TIME = newProperty("crates.out-of-time", "{prefix}<red>You took</red> <green>5 Minutes</green> <red>to open the</red> <gold>{crate}</gold> <red>so it closed.</red>");
+
+    public static final Property<String> CANNOT_BE_VIRTUAL_CRATE = newProperty("crates.cannot-be-a-virtual-crate", "{prefix}<gold>{crate}</gold> <red>cannot be used as a Virtual Crate. You have it set to</red> <gold>{cratetype}.</gold>");
 
     public static final Property<String> OPENED_A_CRATE = newProperty("command.open.opened-a-crate", "{prefix}<gray>You have opened the</gray> <gold>{crate}</gold> <gray>crate for</gray> <gold>{player}.</gold>");
 
@@ -111,15 +121,6 @@ public class Locale implements SettingsHolder {
     public static final Property<String> TAKE_PLAYER_KEYS = newProperty("command.take.take-player-keys", "{prefix}<gray>You have taken</gray> <gold>{amount} key(s)</gold> <gray>from</gray> <gold>{player}.</gold>");
 
     public static final Property<String> TAKE_OFFLINE_PLAYER_KEYS = newProperty("command.take.take-offline-player-keys", "{prefix}<gray>You have taken</gray> <gold>{amount} key(s)</gold> <gray>from the offline player</gray> <gold>{player}.</gold>");
-
-    public static final Property<String> NEED_MORE_ROOM = newProperty("crates.need-more-room", "{prefix}<red>There is not enough space to open {crate} here.</red>");
-
-    public static final Property<String> WORLD_DISABLED = newProperty("crates.world-disabled", "{prefix}<red>Crates are disabled in</red> <gold>{world}.</gold>");
-
-    public static final Property<List<String>> PHYSICAL_CRATE_CREATED = newListProperty("crates.physical-crate.created", List.of(
-            "{prefix}<gray>You have set that block to</gray> <gold>{crate}.</gold>",
-            "{prefix}<gray>To remove</gray> <gold>{crate},</gold> <gray>Shift-click break in creative to remove.</gray>"
-    ));
 
     public static final Property<String> NO_ITEM_IN_HAND = newProperty("command.additem.no-item-in-hand", "{prefix}<red>You need to have an item in your hand to add it to</red> <gold>{crate}.</gold>");
 
