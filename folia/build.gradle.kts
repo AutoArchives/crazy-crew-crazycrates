5bd02f07d5a543fe02d892c372b28d30854f02b8
Ryder Belserion <no-reply@ryderbelserion.com>
2023-08-18 02:06:07 -0400

    add missing tasks

--- folia/build.gradle.kts
+++ folia/build.gradle.kts
@@ -20,4 +20,31 @@ tasks {
             }
         }
     }
+
+    shadowJar {
+        listOf(
+            "dev.triumphteam",
+            "org.jetbrains",
+            "org.bstats",
+            "de.tr7zw"
+        ).forEach {
+            relocate(it, "libs.$it")
+        }
+    }
+
+    processResources {
+        val props = mapOf(
+            "name" to rootProject.name,
+            "group" to project.group,
+            "version" to rootProject.version,
+            "description" to rootProject.description,
+            "authors" to rootProject.properties["authors"],
+            "apiVersion" to "1.20",
+            "website" to "https://modrinth.com/plugin/${rootProject.name.lowercase()}"
+        )
+
+        filesMatching("paper-plugin.yml") {
+            expand(props)
+        }
+    }
 }
