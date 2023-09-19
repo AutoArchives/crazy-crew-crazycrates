cad5ab7517b0dcc24e4adecf2a5791a9990ec22c
Ryder Belserion <no-reply@ryderbelserion.com>
2023-09-19 16:17:14 -0400

    other

--- common/src/main/java/us/crazycrew/crazycrates/common/config/ConfigManager.java
+++ common/src/main/java/us/crazycrew/crazycrates/common/config/ConfigManager.java
@@ -77,7 +77,7 @@ public class ConfigManager {
         // Save en-US.yml
         this.messages.save();
 
-        File messagesFile = new File(this.dataFolder, "/locale/" + pluginConfig.getProperty(PluginConfig.plugin_locale) + ".yml");
+        File messagesFile = new File(this.dataFolder, "/locale/" + this.pluginConfig.getProperty(PluginConfig.plugin_locale) + ".yml");
 
         this.messages = SettingsManagerBuilder
                 .withYamlFile(messagesFile)
