efad79b98e9b447aff301b707847be552c78f11f
Ryder Belserion <no-reply@ryderbelserion.com>
2023-09-19 16:29:20 -0400

    Update changelog with what I've done so far

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -97,12 +97,42 @@ val isSnapshot = rootProject.version.toString().contains("snapshot")
 val file = file("${rootProject.rootDir}/jars/${rootProject.name}-${rootProject.version}.jar")
 
 val description = """
-## New Features:
- * Added back HolographicDisplays support as filoghost has updated it to 1.20.1
- * Added direct support for Oraxen/ItemsAdder
-   * https://docs.crazycrew.us/crazycrates/info/prizes/options#custom-items
- * Added the ability to color maps.
-   * https://docs.crazycrew.us/crazycrates/info/prizes/items/colored-map
+## Changes:
+ * All internal placeholders have been changed from %placeholder% to {placeholder}
+  * This only effects our placeholders like %crate% or %key-amount%, PlaceholderAPI placeholders still use %
+  * This change was made so you can distinguish what is ours and what isn't.
+ * Legacy color codes such as `&7` or `&c` are now deprecated in CrazyCrates.
+  * A toggle has been added to `plugin-config.yml` called `use-minimessage`, It will default to true.
+  * If you still use legacy color codes, Simply set `use-minimessage` to false and reload the plugin.
+ * The plugin prefix is no longer automatically appended to your message, You must use {prefix}.
+ * The messages file has been converted to be in the locale directory called en-US.yml which is just English.
+ * Added a config option to turn off most logging except errors.
+ * Properly turn off metrics when you set the option to false.
+ * `toggle_metrics` and `prefix` have been migrated to `plugin-config.yml`.
+ * Added more verbose logging in places it was needed with the ability to change the logger prefix in `plugin-config.yml`.
+ * All sounds now have a category that respect client side sound settings.
+ * Metrics now track what crate types you are using.
+ * Allow mass open and single open on quickcrate/firecracker.
+ * Updated command permissions to be more simplified, You can view the changes https://docs.crazycrew.us/crazycrates/info/commands/v2/permissions
+  * All permissions except a few default to `false` instead of `op` as I want people to configure their permissions properly.
+ * Removed the admin help command ( We only need one help command, It will show the admin help page if they have the proper permission )
+    
+## Fixes:
+ * Fixed giving keys to offline players.
+
+## Optimizations:
+ * Stopped storing the player object in hashmap's or arraylist's when we didn't need to, We only store the UUID as god intended.
+
+## Developers / APi:
+This is nerd talk so only read this if you need to.
+ * Re-organized code to be a bit more consistent.
+ * Cleaned up the command classes to be more readable
+ * Removed wildcard imports
+ * Added a common module for multi platform purposes
+ * Added adventure api/other dependencies to api/commons module
+  * We set the adventure api to compileOnly so it should always use the platform implementation.
+ * Created an api module with proper interfacing so you no longer need to sift through the main plugin's source code.
+ * You can add, get or check, remove keys from offline and online players.
     
 ## Other:
  * [Feature Requests](https://github.com/Crazy-Crew/${rootProject.name}/issues)
