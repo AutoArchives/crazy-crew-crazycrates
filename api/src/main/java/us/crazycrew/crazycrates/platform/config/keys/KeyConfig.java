b0cf67ed60c29876a72a6526f762453ccf7d7f01
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-13 15:17:51 -0400

    Add config getters

--- api/src/main/java/us/crazycrew/crazycrates/platform/config/keys/KeyConfig.java
+++ api/src/main/java/us/crazycrew/crazycrates/platform/config/keys/KeyConfig.java
@@ -3,6 +3,7 @@ package us.crazycrew.crazycrates.platform.config.keys;
 import org.simpleyaml.configuration.file.YamlConfiguration;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class KeyConfig extends YamlConfiguration {
 
@@ -18,5 +19,29 @@ public class KeyConfig extends YamlConfiguration {
 
     public File getFile() {
         return this.file;
+    }
+
+    public String getMaterial() {
+        return getString("item.material", "TRIPWIRE_HOOK");
+    }
+
+    public String getName() {
+        return getString("item.name", "");
+    }
+
+    public List<String> getLore() {
+        return getStringList("item.lore");
+    }
+
+    public List<String> getItemFlags() {
+        return getStringList("item.flags");
+    }
+
+    public boolean isUnbreakable() {
+        return getBoolean("item.unbreakable", false);
+    }
+
+    public boolean isGlowing() {
+        return getBoolean("item.glowing", true);
     }
 }
