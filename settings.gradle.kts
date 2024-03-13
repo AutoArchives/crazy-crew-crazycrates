4d92e29ed99f4a471687d4ec415115f512a8efe6
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-13 13:43:06 -0400

    Bump cluster api

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -18,7 +18,7 @@ dependencyResolutionManagement {
     versionCatalogs {
         create("libs") {
             version("adventure4", "4.16.0")
-            version("cluster", "6.7")
+            version("cluster", "6.8.1")
 
             library("holographicdisplays", "me.filoghost.holographicdisplays", "holographicdisplays-api").version("3.0.0")
 
