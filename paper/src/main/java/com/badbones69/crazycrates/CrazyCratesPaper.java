a743bcc244e3062073366ea9c36502ce01d952da
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-13 13:44:24 -0400

    Enable cluster api

--- paper/src/main/java/com/badbones69/crazycrates/CrazyCratesPaper.java
+++ paper/src/main/java/com/badbones69/crazycrates/CrazyCratesPaper.java
@@ -1,11 +1,13 @@
 package com.badbones69.crazycrates;
 
 import com.badbones69.crazycrates.platform.PaperServer;
+import com.ryderbelserion.cluster.ClusterPackage;
 import org.bukkit.plugin.java.JavaPlugin;
 import us.crazycrew.crazycrates.CrazyCrates;
 
 public class CrazyCratesPaper extends JavaPlugin {
 
+    private ClusterPackage cluster;
     private CrazyCrates instance;
 
     @Override
@@ -15,11 +17,17 @@ public class CrazyCratesPaper extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        this.cluster = new ClusterPackage(this);
+
         this.instance = new CrazyCrates(new PaperServer());
     }
 
     @Override
     public void onDisable() {
+        if (this.cluster != null) {
+            this.cluster.getCluster().disable();
+        }
+
         if (this.instance != null) {
             this.instance.disable();
         }
