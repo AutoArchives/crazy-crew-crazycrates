c68fa2bf45034537dc0830e8baa3bc92f08adbd2
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-15 20:42:27 -0400

    Add new methods

--- paper/src/main/java/com/badbones69/crazycrates/tasks/crates/CrateManager.java
+++ paper/src/main/java/com/badbones69/crazycrates/tasks/crates/CrateManager.java
@@ -2,16 +2,18 @@ package com.badbones69.crazycrates.tasks.crates;
 
 import com.badbones69.crazycrates.CrazyCratesPaper;
 import com.badbones69.crazycrates.api.objects.Crate;
+import com.badbones69.crazycrates.api.objects.Key;
+import org.bukkit.Location;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.simpleyaml.exceptions.InvalidConfigurationException;
 import us.crazycrew.crazycrates.platform.Server;
 import us.crazycrew.crazycrates.platform.crates.CrateConfig;
+import us.crazycrew.crazycrates.platform.keys.KeyConfig;
+
 import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 import java.util.logging.Level;
 
 public class CrateManager {
@@ -21,8 +23,37 @@ public class CrateManager {
     private final @NotNull Server instance = this.plugin.getInstance();
 
     private final Set<Crate> crates = new HashSet<>();
+    private final Set<Key> keys = new HashSet<>();
+
+    private final Map<UUID, Location> locations = new HashMap<>();
 
     public void load() {
+        File[] keyFilesList = this.instance.getKeyFiles();
+
+        if (keyFilesList == null) {
+            this.plugin.getLogger().severe("Could not read from the keys directory! " + this.instance.getKeyFolder().getAbsolutePath());
+        } else {
+            for (File file : keyFilesList) {
+                this.plugin.getLogger().info("Loading key: " + file.getName());
+
+                KeyConfig keyConfig = new KeyConfig(file);
+
+                try {
+                    keyConfig.load();
+                } catch (InvalidConfigurationException exception) {
+                    this.plugin.getLogger().log(Level.WARNING, file.getName() + " contains invalid YAML structure.", exception);
+                    continue;
+                } catch (IOException exception) {
+                    this.plugin.getLogger().log(Level.WARNING, "Could not load key file: " + file.getName(), exception);
+                    continue;
+                }
+
+                Key key = new Key(keyConfig);
+
+                this.keys.add(key);
+            }
+        }
+
         File[] crateFilesList = this.instance.getCrateFiles();
 
         if (crateFilesList == null) {
@@ -31,7 +62,7 @@ public class CrateManager {
             return;
         }
 
-        for (File file : this.instance.getCrateFiles()) {
+        for (File file : crateFilesList) {
             this.plugin.getLogger().info("Loading crate: " + file.getName());
 
             CrateConfig crateConfig = new CrateConfig(file);
@@ -65,7 +96,40 @@ public class CrateManager {
         return crate;
     }
 
+    public Key getKey(String fileName) {
+        Key key = null;
+
+        for (Key pair : this.keys) {
+            if (!pair.getFileName().equalsIgnoreCase(fileName)) continue;
+
+            key = pair;
+            break;
+        }
+
+        return key;
+    }
+
+    public Set<Key> getKeys() {
+        return Collections.unmodifiableSet(this.keys);
+    }
+
     public Set<Crate> getCrates() {
         return Collections.unmodifiableSet(this.crates);
+    }
+
+    public void addActiveCrate(UUID uuid, Location location) {
+        this.locations.put(uuid, location);
+    }
+
+    public boolean isCrateActive(UUID uuid) {
+        return this.locations.containsKey(uuid);
+    }
+
+    public void removeActiveCrate(UUID uuid) {
+        this.locations.remove(uuid);
+    }
+
+    public Map<UUID, Location> getActiveCrates() {
+        return Collections.unmodifiableMap(this.locations);
     }
 }
