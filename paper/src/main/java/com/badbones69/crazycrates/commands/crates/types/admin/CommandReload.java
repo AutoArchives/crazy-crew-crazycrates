aceba88fae14018910cd4af741ade9a43e7bd332
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-15 22:57:31 -0400

    Setup reload command

--- paper/src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/CommandReload.java
+++ paper/src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/CommandReload.java
@@ -1,34 +1,43 @@
 package com.badbones69.crazycrates.commands.crates.types.admin;
 
+import com.badbones69.crazycrates.api.FileManager;
+import com.badbones69.crazycrates.api.enums.Messages;
+import com.badbones69.crazycrates.api.utils.FileUtils;
 import com.badbones69.crazycrates.commands.crates.BaseCommand;
+import com.badbones69.crazycrates.support.metrics.MetricsManager;
+import com.badbones69.crazycrates.tasks.crates.CrateManager;
 import dev.triumphteam.cmd.bukkit.annotation.Permission;
 import dev.triumphteam.cmd.core.annotations.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.permissions.PermissionDefault;
+import org.jetbrains.annotations.NotNull;
+import us.crazycrew.crazycrates.platform.config.ConfigManager;
+import us.crazycrew.crazycrates.platform.config.impl.ConfigKeys;
 
 public class CommandReload extends BaseCommand {
+
+    private final @NotNull CrateManager crateManager = this.plugin.getCrateManager();
+    private final @NotNull FileManager fileManager = this.plugin.getFileManager();
+    private final @NotNull MetricsManager metrics = this.plugin.getMetrics();
 
     @Command("reload")
     @Permission(value = "crazycrates.reload", def = PermissionDefault.OP)
     public void reload(CommandSender sender) {
-        /*ConfigManager.reload();
+        ConfigManager.reload();
 
-        this.fileManager.reloadAllFiles();
-        this.fileManager.setup();
-
+        FileUtils.cleanFiles();
         FileUtils.loadFiles();
 
         boolean isEnabled = this.config.getProperty(ConfigKeys.toggle_metrics);
 
         if (!isEnabled) {
-            this.plugin.getMetrics().stop();
+            this.metrics.stop();
         } else {
-            this.plugin.getMetrics().start();
+            this.metrics.start();
         }
 
-        FileUtils.cleanFiles();
-
+        /*
         // Close previews
         if (this.config.getProperty(ConfigKeys.take_out_of_preview)) {
             this.plugin.getServer().getOnlinePlayers().forEach(player -> {
@@ -40,9 +49,12 @@ public class CommandReload extends BaseCommand {
                 }
                 //}
             });
-        }
+        }*/
 
-        //this.crateManager.loadCrates();
+        this.fileManager.reloadAllFiles();
+        this.fileManager.setup();
+
+        this.crateManager.load();
 
         if (sender instanceof Player player) {
             player.sendMessage(Messages.reloaded_plugin.getMessage(player));
@@ -50,6 +62,6 @@ public class CommandReload extends BaseCommand {
             return;
         }
 
-        sender.sendMessage(Messages.reloaded_plugin.getMessage());*/
+        sender.sendMessage(Messages.reloaded_plugin.getMessage());
     }
 }
