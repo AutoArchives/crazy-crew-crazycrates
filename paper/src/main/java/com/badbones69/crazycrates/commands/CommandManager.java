bf171b89a32c5e6ef6e1d6546dfc03d7f34e014b
Ryder Belserion <no-reply@ryderbelserion.com>
2024-03-15 21:11:41 -0400

    Convert the rest of command manager

--- paper/src/main/java/com/badbones69/crazycrates/commands/CommandManager.java
+++ paper/src/main/java/com/badbones69/crazycrates/commands/CommandManager.java
@@ -1,9 +1,12 @@
 package com.badbones69.crazycrates.commands;
 
 import com.badbones69.crazycrates.CrazyCratesPaper;
+import com.badbones69.crazycrates.api.objects.other.CrateLocation;
+import com.badbones69.crazycrates.api.utils.FileUtils;
 import com.badbones69.crazycrates.commands.relations.ArgumentRelations;
 import com.badbones69.crazycrates.commands.crates.types.CommandHelp;
 import com.badbones69.crazycrates.commands.crates.types.admin.CommandAdmin;
+import com.badbones69.crazycrates.tasks.crates.CrateManager;
 import dev.triumphteam.cmd.bukkit.BukkitCommandManager;
 import dev.triumphteam.cmd.core.suggestion.SuggestionKey;
 import org.bukkit.command.CommandSender;
@@ -17,6 +20,8 @@ import java.util.List;
 public class CommandManager {
 
     private final static @NotNull CrazyCratesPaper plugin = JavaPlugin.getPlugin(CrazyCratesPaper.class);
+
+    private final static @NotNull CrateManager crateManager = plugin.getCrateManager();
 
     private final static @NotNull BukkitCommandManager<CommandSender> commandManager = BukkitCommandManager.create(plugin);
 
@@ -34,6 +39,14 @@ public class CommandManager {
 
             return numbers;
         });
+
+        getCommandManager().registerSuggestion(SuggestionKey.of("key-types"), (sender, arguments) -> List.of("virtual", "v", "physical", "p"));
+
+        getCommandManager().registerSuggestion(SuggestionKey.of("locations"), (sender, arguments) -> crateManager.getCrateLocations().stream().map(CrateLocation::getID).toList());
+
+        getCommandManager().registerSuggestion(SuggestionKey.of("crates"), (sender, arguments) -> FileUtils.getAllCratesNames());
+
+        //this.bukkitCommandManager.registerArgument(CrateBaseCommand.CustomPlayer.class, (sender, context) -> new CrateBaseCommand.CustomPlayer(context));
 
         List.of(
                 // Admin commands
