3cccd9bfbde1d2147a378a5dc5663ec8faafe59a
Ryder Belserion <no-reply@ryderbelserion.com>
2024-04-07 02:59:36 -0400

    Add paper module

---
+++ paper/src/main/java/com/badbones69/crazycrates/CrazyCrates.java
@@ -0,0 +1,16 @@
+package com.badbones69.crazycrates;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CrazyCrates extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
