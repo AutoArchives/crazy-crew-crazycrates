2cff73c5ceb8569f0a92137188b28473bdf69475
Ryder Belserion <no-reply@ryderbelserion.com>
2024-11-19 02:29:49 -0500

    the plugin SpecializedCrates doesn't boot by default

--- paper/src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/crates/migrator/types/plugins/SpecializedCratesMigrator.java
+++ paper/src/main/java/com/badbones69/crazycrates/commands/crates/types/admin/crates/migrator/types/plugins/SpecializedCratesMigrator.java
@@ -2,6 +2,7 @@ package com.badbones69.crazycrates.commands.crates.types.admin.crates.migrator.t
 
 import com.badbones69.crazycrates.commands.crates.types.admin.crates.migrator.ICrateMigrator;
 import com.badbones69.crazycrates.commands.crates.types.admin.crates.migrator.enums.MigrationType;
+import me.PM2.customcrates.crates.Crate;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.ConfigurationSection;
 import java.io.File;
@@ -14,7 +15,15 @@ public class SpecializedCratesMigrator extends ICrateMigrator {
 
     @Override
     public void run() {
+        final File directory = getCratesDirectory();
 
+        if (!directory.exists()) {
+            directory.mkdirs();
+        }
+
+        Crate.getLoadedCrates().forEach((name, crate) -> {
+            this.plugin.getLogger().warning("Name: " + name);
+        });
     }
 
     @Override
